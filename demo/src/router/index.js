68a7ad9b94e8ce54014110b45022f5df9d2e6d2f
Tatyana Myasnikova <tatyanabak@mail.ru>
2023-01-15 13:31:47 +0700

    Добавлена верстка страницы с доской задач

--- demo/src/router/index.js
+++ demo/src/router/index.js
@@ -1,5 +1,4 @@
 import { createRouter, createWebHistory } from "vue-router";
-import routes from '../routes'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,7 +8,11 @@ const router = createRouter({
       name: "home",
       component: () => import("../components/DemoMenu.vue"),
     },
-    ...routes,
+    {
+      path: "/counter-0",
+      name: "counter-base",
+      component: () => import("../demo0/DemoCounter.vue"),
+    },
   ],
 });
 
