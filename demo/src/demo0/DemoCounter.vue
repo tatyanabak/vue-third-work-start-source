74bbe5712ed1567d28159daa42b809b22dd75377
Tatyana Myasnikova <tatyanabak@mail.ru>
2023-01-17 06:42:24 +0700

    Добавлена логика в компонент со счетчиком, подправлены пути vue-router

--- demo/src/demo0/DemoCounter.vue
+++ demo/src/demo0/DemoCounter.vue
@@ -1,9 +1,25 @@
 <template>
     <div>
-        <button> - </button>
+        <button @click="decrement"> - </button>
 
-        <span> Current value: 0 </span>
+        <span> Current value: {{ count }} </span>
 
-        <button> + </button>
+        <button @click="increment"> + </button>
     </div>
 </template>
+
+<script setup>
+import { ref } from 'vue'
+
+const count = ref(0)
+
+// метод уменьшения значения
+const decrement = () => {
+    count.value = count.value - 1
+}
+
+// метод увеличения значения
+const increment = () => {
+    count.value = count.value + 1
+}
+</script>
--- demo/src/routes.js
+++ demo/src/routes.js
@@ -1,6 +1,8 @@
 export default [
-    {
-      name: 'Counter',
-      link: '/counter-0'
-    }
-  ]
+  {
+    name: 'Counter',
+    // link: '/counter-0', - отображено в демке 1.14. Создание шаблона для компонента счётчика
+    path: '/counter-0', // взято с гитхаба
+    // component: () => import("./demo0/DemoCounter.vue"), взято с гитхаба
+  }
+];
